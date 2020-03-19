28d6c55039a433e203d121da1f61501bef01d3ee
muhamob <yusov.aa@phystech.edu>
2020-03-19 15:52:05 +0300

    Add loader and example

---
+++ src/mongo/loader.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+import typing
+
+
+class Saver:
+    def __init__(self, client: MongoClient, db_name: str, collection_name: str):
+        self.__client: MongoClient = client
+        self.__db = client[db_name]
+        self.__collection = self.__db[collection_name]
+
+    def save(self, obj: typing.Union[dict, typing.Sequence[dict]]):
+        if isinstance(obj, dict):
+            self.__collection.insert_one(obj)
+        elif isinstance(obj, (list, tuple)):
+            self.__collection.insert_many(obj)
+        else:
+            raise ValueError(f"Obj must be either dict or sequence, got {type(obj)}")
+
+
+if __name__ == "__main__":
+    import glob
+    import json
+    from tqdm import tqdm
+
+    client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=1000)
+    xlsx_saver = Saver(client, 'main', 'xlsx')
+
+    for path in tqdm(glob.glob("../../data/xlsx-parsed/*.json")):
+        with open(path, 'r') as f:
+            obj = json.load(f)
+
+        assert isinstance(obj, (dict, list, tuple)), f"Obj must be dict, tuple or list. Got {type(obj)}"
+        xlsx_saver.save(obj)
