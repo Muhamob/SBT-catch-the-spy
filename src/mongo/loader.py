edae03af5930e273919d04ce19a52ee309ffa7cc
muhamob <yusov.aa@phystech.edu>
2020-03-23 20:27:18 +0300

    Update: rename Saver -> Loader

--- src/mongo/loader.py
+++ src/mongo/loader.py
@@ -3,13 +3,13 @@ from pymongo import MongoClient
 import typing
 
 
-class Saver:
+class Loader:
     def __init__(self, client: MongoClient, db_name: str, collection_name: str):
         self.__client: MongoClient = client
         self.__db = client[db_name]
         self.__collection = self.__db[collection_name]
 
-    def save(self, obj: typing.Union[dict, typing.Sequence[dict]]):
+    def load(self, obj: typing.Union[dict, typing.Sequence[dict]]):
         if isinstance(obj, dict):
             self.__collection.insert_one(obj)
         elif isinstance(obj, (list, tuple)):
@@ -24,11 +24,11 @@ if __name__ == "__main__":
     from tqdm import tqdm
 
     client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=1000)
-    xlsx_saver = Saver(client, 'main', 'xlsx')
+    xlsx_saver = Loader(client, 'main', 'xlsx')
 
     for path in tqdm(glob.glob("../../data/xlsx-parsed/*.json")):
         with open(path, 'r') as f:
             obj = json.load(f)
 
         assert isinstance(obj, (dict, list, tuple)), f"Obj must be dict, tuple or list. Got {type(obj)}"
-        xlsx_saver.save(obj)
+        xlsx_saver.load(obj)
